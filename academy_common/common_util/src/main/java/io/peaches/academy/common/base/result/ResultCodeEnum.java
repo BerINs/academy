49bde20ea4b6cf32c5e5fe5afa772fd8191b6d6c
berior <1269921156@qq.com>
2022-03-27 00:27:37 +0800

    在线学院

--- academy_common/common_util/src/main/java/io/peaches/academy/common/base/result/ResultCodeEnum.java
+++ academy_common/common_util/src/main/java/io/peaches/academy/common/base/result/ResultCodeEnum.java
@@ -55,7 +55,8 @@ public enum ResultCodeEnum {
     LOGIN_AUTH(false, 28004, "需要登录"),
     LOGIN_ACL(false, 28005, "没有权限"),
     SMS_SEND_ERROR(false, 28006, "短信发送失败"),
-    SMS_SEND_ERROR_BUSINESS_LIMIT_CONTROL(false, 28007, "短信发送过于频繁");
+    SMS_SEND_ERROR_BUSINESS_LIMIT_CONTROL(false, 28007, "短信发送过于频繁"),
+    MY_VALUE(false, 30000, "我的错误");
 
 
     private Boolean success;
